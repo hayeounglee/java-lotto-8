0e0a76608e2e04e571a87578c63f51267ba8850b
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2025-11-01 00:57:03 +0900

    feat: add class for bonus number

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -1,2 +1,16 @@
-package lotto.model;public class Bonus {
+package lotto.model;
+
+public class Bonus {
+    private final static int MIN_RANGE = 1;
+    private final static int MAX_RANGE = 45;
+
+    private final int bonus;
+
+    public Bonus(int num, Lotto lotto) {
+        bonus = num;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
 }
